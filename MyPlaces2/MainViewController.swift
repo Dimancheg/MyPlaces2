28a2fe3582d14b2661ad9a2fd58c28804d83641d
kalnitsky80@gmail.com <kalnitsky80@gmail.com>
2023-01-22 10:28:45 +0200

    Initial Commit

---
+++ MyPlaces2/MainViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MainViewController.swift
+//  MyPlaces2
+//
+//  Created by Dmitry Kalnitsky on 22/01/2023.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    
+    let parkName = ["Парк Бэ-Иврит", "Парк А-Манхигим", "Парк Ган-аир"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+       return parkName.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = parkName[indexPath.row]
+        cell.imageView?.image = UIImage(named: parkName[indexPath.row])
+
+        return cell
+    }
+   
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyPlaces2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces2
+//
+//  Created by Dmitry Kalnitsky on 22/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
